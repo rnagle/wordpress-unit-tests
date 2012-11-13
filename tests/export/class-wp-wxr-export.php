30944b10bce4f062f49a00282ccf4099ad80ecbe
nbachiyski <nbachiyski@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2012-11-13 18:02:47 +0000

    Update renamed method name

--- tests/export/class-wp-wxr-export.php
+++ tests/export/class-wp-wxr-export.php
@@ -140,15 +140,15 @@ class Test_WP_WXR_Export extends WP_UnitTestCase {
 		_unregister_post_type( 'baba' );
 	}
 
-	function test_export_using_writer_class_returns_wp_error_if_the_writer_throws_wxr_exception() {
+	function test_export_xml_using_writer_class_returns_wp_error_if_the_writer_throws_wxr_exception() {
 		$export = new WP_WXR_Export();
-		$this->assertTrue( is_wp_error( $export->export_using_writer_class( 'Test_WP_WXR_Stub_Writer_Throws_WXR_Exception' ) ) );
+		$this->assertTrue( is_wp_error( $export->export_xml_using_writer_class( 'Test_WP_WXR_Stub_Writer_Throws_WXR_Exception' ) ) );
 	}
 
-	function test_export_using_writer_class_passes_the_exception_if_the_writer_throws_other_exception() {
+	function test_export_xml_using_writer_class_passes_the_exception_if_the_writer_throws_other_exception() {
 		$this->setExpectedException( 'Exception' );
 		$export = new WP_WXR_Export();
-		$export->export_using_writer_class( 'Test_WP_WXR_Stub_Writer_Throws_Other_Exception' );
+		$export->export_xml_using_writer_class( 'Test_WP_WXR_Stub_Writer_Throws_Other_Exception' );
 	}
 
 	function test_authors_should_return_list_of_users_for_each_post_author() {
