97505eb89f12251eece9ac697589b73ef9f6a0f0
kurtpayne <kurtpayne@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2013-01-04 00:22:48 +0000

    Fixing the location detection for jQuery from [23183]. This will now detect that jQuery is a series of 2 dependencies, and verify that each one is referenced correctly.

--- tests/dependencies/jquery.php
+++ tests/dependencies/jquery.php
@@ -7,11 +7,21 @@
 class Tests_Dependencies_jQuery extends WP_UnitTestCase {
 
 	function test_location_of_jquery() {
+        $jquery_scripts = array(
+            'jquery-core'    => '/wp-includes/js/jquery/jquery.js',
+            'jquery-migrate' => '/wp-includes/js/jquery/jquery-migrate.js'
+        );
 		$scripts = new WP_Scripts;
 		wp_default_scripts( $scripts );
 		$object = $scripts->query( 'jquery', 'registered' );
 		$this->assertInstanceOf( '_WP_Dependency', $object );
-		$this->assertEquals( '/wp-includes/js/jquery/jquery.js', $object->src );
+        $this->assertEqualSets( $object->deps, array_keys( $jquery_scripts ) );
+        foreach( $object->deps as $dep ) {
+            $o = $scripts->query( $dep, 'registered' );
+            $this->assertInstanceOf( '_WP_Dependency', $object );
+            $this->assertTrue( isset( $jquery_scripts[ $dep ] ) );
+            $this->assertEquals( $jquery_scripts[ $dep ], $o->src );
+        }
 	}
 
 	function test_presence_of_jquery_no_conflict() {
