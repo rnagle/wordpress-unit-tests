a27b94988a75afa31ff3920319827ac637cae182
joehoyle <joehoyle@4a5a2adb-35eb-4268-9a1b-fe04a4134342>
2013-01-14 21:25:14 +0000

    Added some more tests for WP_Image_Editor_GD for crop(), rotate() etc

--- tests/image/editor_gd.php
+++ tests/image/editor_gd.php
@@ -4,6 +4,7 @@
  * Test the WP_Image_Editor_GD class
  * @group image
  * @group media
+ * @group wp-image-editor-gd
  */
 
 class Tests_Image_Editor_GD extends WP_Image_UnitTestCase {
@@ -13,6 +14,106 @@ class Tests_Image_Editor_GD extends WP_Image_UnitTestCase {
 		require_once( ABSPATH . WPINC . '/class-wp-image-editor.php' );
 		require_once( ABSPATH . WPINC . '/class-wp-image-editor-gd.php' );
 		parent::setUp();
+	}
+
+	/**
+	 * Check support for GD compatible mime types.
+	 * 
+	 */
+	public function test_supports_mime_type() {
+		$gd_image_editor = new WP_Image_Editor_GD( null );
+
+		$this->assertTrue( $gd_image_editor->supports_mime_type( 'image/jpeg' ), 'Does not support image/jpeg' );
+		$this->assertTrue( $gd_image_editor->supports_mime_type( 'image/png' ), 'Does not support image/png' );
+		$this->assertTrue( $gd_image_editor->supports_mime_type( 'image/gif' ), 'Does not support image/gif' );
+	}
+
+	/**
+	 * Test resizing an image, not using crop
+	 * 
+	 */
+	public function test_resize() {
+
+		$file = DIR_TESTDATA . '/images/gradient-square.jpg';
+
+		$gd_image_editor = new WP_Image_Editor_GD( $file );
+		$gd_image_editor->load();
+
+		$gd_image_editor->resize( 100, 50 );
+
+		$this->assertEquals( array( 'width' => 50, 'height' => 50 ), $gd_image_editor->get_size() );
+	}
+
+	/**
+	 * Test resizing an image including cropping
+	 * 
+	 */
+	public function test_resize_and_crop() {
+
+		$file = DIR_TESTDATA . '/images/gradient-square.jpg';
+
+		$gd_image_editor = new WP_Image_Editor_GD( $file );
+		$gd_image_editor->load();
+
+		$gd_image_editor->resize( 100, 50, true );
+
+		$this->assertEquals( array( 'width' => 100, 'height' => 50 ), $gd_image_editor->get_size() );
+	}
+
+	/**
+	 * Test cropping an image
+	 */
+	public function test_crop() {
+
+		$file = DIR_TESTDATA . '/images/gradient-square.jpg';
+
+		$gd_image_editor = new WP_Image_Editor_GD( $file );
+		$gd_image_editor->load();
+
+		$gd_image_editor->crop( 0, 0, 50, 50 );
+
+		$this->assertEquals( array( 'width' => 50, 'height' => 50 ), $gd_image_editor->get_size() );
+
+	}
+
+	/**
+	 * Test rotating an image 180 deg
+	 */
+	public function test_rotate() {
+
+		$file = DIR_TESTDATA . '/images/gradient-square.jpg';
+
+		$gd_image_editor = new WP_Image_Editor_GD( $file );
+		$gd_image_editor->load();
+
+		$property = new ReflectionProperty( $gd_image_editor, 'image' );
+		$property->setAccessible( true );
+
+		$color_top_left = imagecolorat( $property->getValue( $gd_image_editor ), 0, 0 );
+
+		$gd_image_editor->rotate( 180 );
+
+		$this->assertEquals( $color_top_left, imagecolorat( $property->getValue( $gd_image_editor ), 99, 99 ) );
+	}
+
+	/**
+	 * Test flipping an image
+	 */
+	public function test_flip() {
+
+		$file = DIR_TESTDATA . '/images/gradient-square.jpg';
+
+		$gd_image_editor = new WP_Image_Editor_GD( $file );
+		$gd_image_editor->load();
+
+		$property = new ReflectionProperty( $gd_image_editor, 'image' );
+		$property->setAccessible( true );
+
+		$color_top_left = imagecolorat( $property->getValue( $gd_image_editor ), 0, 0 );
+
+		$gd_image_editor->flip( true, false );
+
+		$this->assertEquals( $color_top_left, imagecolorat( $property->getValue( $gd_image_editor ), 0, 99 ) );
 	}
 
 	/**
